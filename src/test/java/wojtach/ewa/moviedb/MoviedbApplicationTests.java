2442ddd39bab07a3476e792c7e3bc24c9fcbff51
ewa <ewa.listy@gmail.com>
2017-04-16 14:05:47 +0200

    cmd test run

--- src/test/java/wojtach/ewa/moviedb/MoviedbApplicationTests.java
+++ src/test/java/wojtach/ewa/moviedb/MoviedbApplicationTests.java
@@ -3,10 +3,12 @@ package wojtach.ewa.moviedb;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+
 public class MoviedbApplicationTests {
 
 	@Test
